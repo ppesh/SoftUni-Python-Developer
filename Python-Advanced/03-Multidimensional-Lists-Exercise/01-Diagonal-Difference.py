27f8cb88129b65e15a11aec6cdea115eb59bdfd7
ppesh <63230094+ppesh@users.noreply.github.com>
2021-01-19 10:52:00 +0100

    Create 01-Diagonal-Difference.py

---
+++ Python-Advanced/03-Multidimensional-Lists-Exercise/01-Diagonal-Difference.py
@@ -0,0 +1,23 @@
+# 01. Diagonal Difference
+
+n = int(input())
+
+matrix = []
+
+for _ in range(n):
+	line = [int(x) for x in input().split()]
+	matrix.append(line)
+
+# primary diagonal	
+primary_sum = 0
+for row in range(n):
+	col = row
+	primary_sum += matrix[row][col]
+
+# secondary diagonal 	
+secondary_sum = 0
+for row in range(n):
+	col = n - 1 - row
+	secondary_sum += matrix[row][col]
+	
+print(abs(primary_sum - secondary_sum))
