72a32097f547183779494f47dac0f42bc76e79b4
ppesh <63230094+ppesh@users.noreply.github.com>
2021-01-27 10:35:22 +0100

    Comprehensions

---
+++ Python-Advanced/04-Comprehensions-Exercise/03-Capitals.py
@@ -0,0 +1,9 @@
+# 03. Capitals
+
+countries = input().split(", ")
+capitals = input().split(", ")
+
+result = {country: capital for country, capital in zip(countries, capitals)}
+[print(f"{key} -> {value}") for key, value in result.items()]
+
+
