cef68f440c673c45ec89a0800ae4eeb9357525ec
ppesh <63230094+ppesh@users.noreply.github.com>
2021-01-28 22:19:33 +0100

    Comprehensions

---
+++ Python-Advanced/04-Comprehensions-Exercise/06-Matrix-of-Palindromes.py
@@ -0,0 +1,9 @@
+# 06. Matrix of Palindromes
+
+rows, cols = [int(x) for x in input().split()]
+
+matrix = [[f"{chr(row)}{chr(row+col)}{chr(row)}" for col in range(cols)] for row in range(97, 97+rows)]
+
+[print(' '.join(row)) for row in matrix]
+
+
