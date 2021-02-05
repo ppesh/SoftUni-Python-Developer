10ffb4fc481b852f6373de50c6604ab50ed79423
ppesh <63230094+ppesh@users.noreply.github.com>
2021-02-05 21:23:35 +0100

    Functions Advanced

---
+++ Python-Advanced/05-Functions-Advanced-Exercise/03-Min-Max-and-Sum.py
@@ -0,0 +1,16 @@
+# 03. Min Max and Sum
+
+input_num = [int(x) for x in input().split()]
+
+print(f"The minimum number is {min(input_num)}")
+print(f"The maximum number is {max(input_num)}")
+print(f"The sum number is: {sum(input_num)}")
+
+
+
+
+
+
+
+
+
