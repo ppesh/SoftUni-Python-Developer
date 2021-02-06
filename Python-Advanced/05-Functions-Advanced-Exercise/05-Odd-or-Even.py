b2b9141368fdccd8f82003a712d54f63eef997e3
ppesh <63230094+ppesh@users.noreply.github.com>
2021-02-06 10:24:50 +0100

    Functions Advanced

---
+++ Python-Advanced/05-Functions-Advanced-Exercise/05-Odd-or-Even.py
@@ -0,0 +1,21 @@
+# 05. Odd or Even
+
+command = input()
+numbers = list(map(int, input().split()))
+result = 0
+
+if command == "Odd":
+	result = sum(filter(lambda x: x % 2 == 1, numbers)) * len(numbers)
+else:
+	result = sum(filter(lambda x: x % 2 == 0, numbers)) * len(numbers)
+	
+print(result)
+
+
+
+
+
+
+
+
+
