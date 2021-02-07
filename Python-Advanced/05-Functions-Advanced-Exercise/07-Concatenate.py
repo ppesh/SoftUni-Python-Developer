394ff05fff4eaf48109e9d1aec1d323f2a692a4c
ppesh <63230094+ppesh@users.noreply.github.com>
2021-02-07 12:02:45 +0100

    Functions Advanced

---
+++ Python-Advanced/05-Functions-Advanced-Exercise/07-Concatenate.py
@@ -0,0 +1,20 @@
+# 07. Concatenate
+
+def concatenate(*args):
+    result = ''
+    for arg in args:
+        result += arg
+    return result
+
+        
+# Test Code:
+print(concatenate("Soft", "Uni", "Is", "Great", "!"))
+
+
+
+
+
+
+
+
+
